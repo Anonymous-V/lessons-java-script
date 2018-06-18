edfb028e09adb9d01d7b638b8d5eaf8736f28c87
Anonymous-V <suleymanovvitaliy349@gmail.com>
2018-06-18 21:00:52 +0300

    Второй урок (дополнительно)

---
+++ lesson2.addition1/script.js
@@ -0,0 +1,29 @@
+/*let x = 5;
+alert(x++); // 5
+
+console.log([] + false - null + true); // NaN
+
+let y = 1;
+let x1 = y = 2;
+alert(x1); // 2
+
+console.log([] + 1 + 2); // 12
+
+alert("1"[0]); // 1
+
+console.log(2 && 1 && null && 0 && undefined); // null
+
+console.log(!!(1 && 1)); // true
+console.log( (1 && 1) ); // 1
+
+alert(null || 2 && 3 || 4); // 3
+
+let a = [1, 2, 3];
+let b = [1, 2, 3];
+console.log(a == b); // false
+
+alert(+ "Infinity"); // Infinity
+
+console.log("ёжик" > "яблоко"); // true
+
+console.log(0 || "" || 2 || undefined || true || false); // 2*/
