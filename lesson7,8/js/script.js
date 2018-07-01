69044e43cbfb8042776e25214d1a623c4904e4cb
Anonymous-V <suleymanovvitaliy349@gmail.com>
2018-07-01 14:14:57 +0300

    Урок 9 (обязательно)

--- lesson7,8/js/script.js
+++ lesson7,8/js/script.js
@@ -138,4 +138,30 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 
+
+	// Modal window
+
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close'),
+		description_btn = document.querySelectorAll('.description-btn');
+
+	function showModalWindow() {
+		this.classList.add('more-splash');
+		overlay.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+	}
+
+	for (let i = 0; i < description_btn.length; ++i) {
+		description_btn[i].addEventListener('click', showModalWindow)
+	}
+
+	more.addEventListener('click', showModalWindow);
+
+	close.addEventListener('click', function () {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+
 });
