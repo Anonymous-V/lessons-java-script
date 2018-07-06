7ef1fd20e803710eef08781d3b95ba168526ece1
Anonymous-V <suleymanovvitaliy349@gmail.com>
2018-07-06 12:52:59 +0300

    Двенадцатый урок (необязательный)

--- Homework12/js/script.js
+++ Homework12/js/script.js
@@ -1,13 +1,63 @@
 $(document).ready(function () {
 
 	$(".main_btna, .main_btn, a[href='#sheldure']").click(function() {
-		$('.modal').slideDown('slow');
-		$('.overlay').fadeToggle('slow');
+		$('.modal').animate({
+			opacity: 'toggle',
+			// left: "200px", 
+			top: "10%"
+		}, 1000);
+
+		$('.overlay').animate({
+			opacity: 'toggle'
+		}, 1500);
 	});
 
 	$('.close').click(function() {
-		$('.modal').fadeToggle('slow');
-		$('.overlay').slideUp('slow');
+		$('.modal').animate({
+			top: '50%',
+			opacity: 'toggle'
+		}, 1000);
+
+		$('.overlay').animate({
+			opacity: 'toggle'
+		}, 1500);
+
+		$('#allGood').animate({
+			opacity: 'toggle',
+		}, 1000).remove();
+			
+	});
+
+	$('form').submit(function(event) {
+		event.preventDefault();
+
+		let form = $(this);
+		$.ajax({
+			url: 'server.php',
+			type: 'POST',
+			data: form.serialize(),
+		})
+		.done(function() {
+			console.log("success");
+			$('<div>', {
+				text: 'All good!',
+				id: 'allGood',
+				css: {
+					marginTop: '20px',
+					textAlign: 'center',
+					fontSize: '1.3em',
+				}
+			}).appendTo('form');
+		})
+		.fail(function(jqXHR, textStatus) {
+			console.log(jqXHR.statusText);
+		})
+		.always(function() {
+			console.log("complete");
+		});
+
+		$(form).trigger('reset');
+		
 	});
 
 });
--- lesson7,8,11/js/script.js
+++ lesson7,8,11/js/script.js
@@ -169,18 +169,60 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	let message = new Object();
 	message.loading = "Загрузка...";
-	message.success = "Спасибо! Скоро мы свяжемся с Вами";
+	message.success = 'Спасибо! Скоро мы свяжемся с Вами';
 	message.failure = "Что-то пошло не так...";
 
 	let form = document.getElementsByClassName('main-form')[0],
+		formContact = document.getElementById('form'),
 		input = form.getElementsByTagName('input'),
+		inputContact = formContact.getElementsByTagName('input'),
 		statusMessage = document.createElement('div');
 
 	statusMessage.classList.add('status');
 
-	form.addEventListener('sumbit', function (event) {
+	form.addEventListener('submit', callForm);
+	formContact.addEventListener('submit', callForm);
+
+	function callForm(event) {
 		event.preventDefault();
-		form.appendChild(statusMessage);
-	});
+
+		let formName, inp;
+		if (event.target === formContact) {
+			formName = formContact;
+			inp = inputContact;
+		} else if (event.target === form) {
+			formName = form;
+			inp = input;
+		}
+
+		formName.appendChild(statusMessage);
+
+		// AJAX
+		let request = new XMLHttpRequest();
+		request.open("POST", 'server.php');
+
+		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		let formData = new FormData(formName);
+
+		request.send(formData);
+
+		request.onreadystatechange = function () {
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			}	else if (request.readyState === 4) {
+				if (request.status == 200 && request.status < 300) {
+					statusMessage.innerHTML = message.success;
+					// Добавляем контент на страницу
+				} else {
+					statusMessage.innerHTML = message.failure;
+				}
+			}
+		}
+
+		for (let i = 0; i < inp.length; ++i) {
+			inp[i].value = '';
+		}
+	}
 
 });
