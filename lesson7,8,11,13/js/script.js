e2eb7e4de43634ee4bedd5d1e91b9513184ca740
Anonymous-V <suleymanovvitaliy349@gmail.com>
2018-07-07 14:26:30 +0300

    Тринадцатый урок

--- lesson7,8,11,13/js/script.js
+++ lesson7,8,11,13/js/script.js
@@ -225,4 +225,109 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 
+
+	// Slider
+
+	let slideIndex = 1,
+		slides = document.getElementsByClassName('slider-item'),
+		prev = document.querySelector('.prev'),
+		next = document.querySelector('.next'),
+		dotsWrap = document.querySelector('.slider-dots'),
+		dots = document.getElementsByClassName('dot');
+
+	showSlides(slideIndex);
+
+	function showSlides(index) {
+		if (index > slides.length) {
+			slideIndex = 1;
+		} else if (index < 1) {
+			slideIndex = slides.length;
+		};
+
+
+		for (let i = 0; i < slides.length; ++i) {
+			slides[i].style.display = 'none';
+			dots[i].classList.remove('dot-active');
+		}
+
+		slides[slideIndex - 1].style.display = 'block';
+		dots[slideIndex - 1].classList.add('dot-active'); 
+	}
+
+	function plusSlides(index) {
+		showSlides(slideIndex += index);
+	}
+
+	function currentSlide(index) {
+		showSlides(slideIndex = index);
+	}
+
+	prev.addEventListener('click', function () {
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', function () {
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener('click', function (event) {
+		for (let i = 0; i < dots.length + 1; ++i) {
+			if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+				currentSlide(i);
+			}
+		}
+	});
+
+	let startSlider = setInterval(function () {
+		plusSlides(1);
+	}, 5000);
+
+
+	// Calculator
+
+	let persons = document.getElementsByClassName('counter-block-input')[0],
+		restDay = document.getElementsByClassName('counter-block-input')[1],
+		place = document.getElementById('select'),
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
+
+	totalValue.innerHTML = 0;
+
+	persons.addEventListener('change', function () {
+		personsSum = this.value;
+		total = ( parseInt(daysSum, 10) + parseInt(personsSum, 10) ) * 4000;
+
+		if ( isNaN(parseInt(restDay.value, 10)) || isNaN(parseInt(persons.value, 10)) ) {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	restDay.addEventListener('change', function () {
+		daysSum = this.value;
+		total = ( parseInt(daysSum, 10) + parseInt(personsSum, 10) ) * 4000;
+
+
+		if ( isNaN(parseInt(persons.value, 10))  ||  isNaN(parseInt(restDay.value, 10))) {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	place.addEventListener('change', function () {
+		let rest = restDay.value;
+		let pers = persons.value;
+
+		if ( isNaN(parseInt(rest, 10)) || isNaN(parseInt(pers, 10)) ) {
+			totalValue.innerHTML = 0;
+		} else {
+			let a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+	});
+
 });
